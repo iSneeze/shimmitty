83cca244c97ffadf1ba007678714af78fa39683f
iSneeze <yeetallthespam@gmail.com>
2025-01-04 20:01:26 +0100

    added bulk actions extension

--- data/config/extensions.conf.php
+++ data/config/extensions.conf.php
@@ -1,2 +1,2 @@
 <?php
-define("EXTRA_EXTS", "approval,auto_tagger,autocomplete,emoticons,emoticons_list,favorites,featured,filter,handle_video,home,image_view_counter,ipban,log_console,not_a_tag,numeric_score,pools,rating,relationships,rss_images,tag_categories,tag_tools,transcode_video,trash");
+define("EXTRA_EXTS", "approval,auto_tagger,autocomplete,bulk_actions,emoticons,emoticons_list,favorites,featured,filter,handle_video,home,image_view_counter,ipban,log_console,not_a_tag,numeric_score,pools,rating,relationships,rss_images,tag_categories,tag_tools,transcode_video,trash");
